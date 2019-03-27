2aaefef874d0ba505f9f7610092afa261c70f4d2
Julian <milch44@googlemail.com>
2019-03-27 13:11:50 +0100

    price change input and update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: JSPlug
+Plugin Name: Custom Price Changer for WooCommerce 
 Plugin UI:
 Description: Simple plugin
 Author: Julian Schreiner
@@ -24,27 +24,21 @@ function test(){
 
 
 function addMenu(){
-	add_menu_page("Example Options", "JS Plug Options", "4", "ex-options", "exampleMenu");
+	add_menu_page("Example Options", "Price Changer", "4", "ex-options", "exampleMenu");
 	
 }
 
 function exampleMenu(){
 	$t = <<< TPL
          <div class="wrap">
-         	<h1>Product Custom Prices</h1>
-         	<form method="POST" action="">
-         		<input type="submit" name="create_shortcode" value="Show All Products" class="button-primary">
-         	</form>
+         	<h1>Custom Product Prices</h1>
          </div> 	 
 TPL;
-    echo $t;
+	echo $t;
 
 
-
-    if(isset($_POST['create_shortcode'])){
-    	handlePost();
-    }
-
+		handlePost();
+			
 }
 
 /*
@@ -52,7 +46,12 @@ TPL;
 */
 
 function handlePost(){
-	global $wpdb;
+	global $wpdb, $wp_roles;
+
+	foreach($wp_roles->roles as $role){
+		echo '<input type="hidden" name="'. $role['name'] .'" class="roles"></input>'; 
+		//var_dump($role['name']);
+	}
 	
 	$mydrafts = $wpdb->get_results(
 		"
@@ -93,15 +92,40 @@ GROUP BY p.ID
 		echo '</thead>';
 		echo '<tbody id="the-list">';
 
+
+
 	foreach ($mydrafts as $draft){
-		echo '<tr id="post-'.$draft->ID.'" class="iedit author-self level-0 post-136 type-product status-publish has-post-thumbnail hentry product_cat-allgemein">';
+		$sku = $wpdb->get_results(
+				"
+				SELECT meta_value 
+				FROM wp_postmeta
+				WHERE meta_key = '_sku' AND post_id =" . $draft->ID	
+		);
+		$sku = $sku[0];
+
+		$where = 'price_change_product_' . $sku->meta_value;
+
+		$priceChangeInformations = $wpdb->get_results(
+			"SELECT * FROM wp_posts WHERE post_title = '".$where."'"
+		);
+
+		$priceChangeInformations = $priceChangeInformations[0];
+		
+
+
+		$description = substr($draft->Description, 0, 100);
+		$description .= '...';
+
+
+		echo '<tr id="post-'.$sku->meta_value.'" class="iedit author-self level-0 post-136 type-product status-publish has-post-thumbnail hentry product_cat-allgemein">';
 		echo '<td class="name column-name has-row-actions column-primary">' . $draft->Name . '</td>';
-		echo '<td class="name column-name has-row-actions column-primary">' . $draft->SKU . '</td>';
+		echo '<td class="name column-name has-row-actions column-primary" id="sku">' . $sku->meta_value . '</td>';
 		echo '<td class="name column-name has-row-actions column-primary">' . $draft->Price . '</td>';
-		echo '<td class="name column-name has-row-actions column-primary">'. $draft->Description . '</td>';
+		echo '<td class="name column-name has-row-actions column-primary">'. $description . '</td>';
+		echo '<input type="hidden" name="priceInformation'.$sku->meta_value.'" value="'. htmlspecialchars($priceChangeInformations->post_content) .'"></input>';
 		echo '<td class="name column-name has-row-actions column-primary">
 				<input type="submit" class="button-primary" 
-				value="Edit" name="'.$draft->ID.'"
+				value="Edit" name="'.$sku->meta_value.'"
 				></button></td>';
 
 		/*echo '<tr>
@@ -116,11 +140,83 @@ GROUP BY p.ID
 	}	
 	echo '</tbody>';
 	echo '</table>';
+
+	if(isset($_POST['editPrices'])){
+		foreach($wp_roles->roles as $role){
+			//var_dump($role['name']);
+
+			$rol = $role['name'];
+
+			$rol = str_replace(' ', '_', $rol);
+
+			//var_dump($_POST[$rol . 'Price']);
+		}
+		
+		$jsonArr = [];
+
+		foreach($_POST as $key => $value){
+			$jsonArr[$key] = $value;
+		}
+
+		echo json_encode($jsonArr);
+
+		$jsonArr = json_encode($jsonArr);
+		
+		$mysql_date_now = date("Y-m-d H:i:s");
+		$sku = $_POST['productID'];
+		$where = 'price_change_product_' . $sku;
+
+
+		$alreadyInDB = $wpdb->get_results(
+			"SELECT * FROM wp_posts WHERE post_title = '".$where."'"
+		);
+
+
+
+		// TODO CHECK IF ITS IN DB ALREADY
+		if (isset($alreadyInDB) && empty($alreadyInDB)){
+			// INSERT INTO DB
+			$result = $wpdb->insert('wp_posts', array(
+					'post_author' => 1,
+					'post_date' => $mysql_date_now,
+					'post_date_gmt' => $mysql_date_now,
+					'post_content' => $jsonArr,
+					'post_title' => $where,
+					'post_status' => 'plugin',
+					'post_name'	=> 'price_changer'
+			));
+		}
+		else{
+			// UPDATE ENTRY
+			$result = $wpdb->update('wp_posts', array(
+				'post_date' => $mysql_date_now,
+				'post_date_gmt' => $mysql_date_now,
+				'post_content' => $jsonArr
+			), array('post_title' => $where));
+		}
+
+		
+	}
 }
 
 // HERE below in the array set your specific product IDs
 function specific_product_ids(){
-    return array(136); //  <===  <===  <===  <===  Your Product IDs
+		global $wpdb;
+		$sku = $wpdb->get_results(
+			"
+			SELECT meta_value 
+			FROM wp_postmeta
+			WHERE meta_key = '_sku'"
+		);
+		
+		$retArray = [];
+
+		foreach($sku as $nr){
+			$retArray[] = (int) $nr->meta_value;
+		}
+
+
+		return $numerical;
 }
 
 
@@ -138,11 +234,13 @@ function get_price_multiplier() {
 
 function custom_price( $price, $product ) {
 	if( in_array($product->get_id(), specific_product_ids() ) ) {
+		// TODO CHECK USER GROUP AND GIVE SETTED PRICE FOR THIS PRODUCT!!
     	return $price * get_price_multiplier();
 	} else{
 		return $price;
 	}
 }
+
 
 
 
--- js/script.js
+++ js/script.js
@@ -4,7 +4,63 @@ jQuery( document ).ready(function($) {
 
     $('.button-primary').click(function() {
     	console.log("clicked");
-    	console.log(this.name);
-    	
+        console.log(this.name);
+        
+        let roles = [];
+
+        $('input[type=hidden].roles').each(function(){
+            roles.push(this.name);
+        });
+        
+        console.log(roles);
+        
+        let priceInformations = $('input[type=hidden][name=priceInformation'+ this.name +']').val();
+        console.log(priceInformations);
+
+        var obj = JSON.parse(priceInformations);
+        console.log(obj);
+        let template = '';
+
+        console.log(Object.keys(obj).length);
+
+        var length = Object.keys(obj).length;
+
+        if(length > 1){
+            roles.forEach(function(element){
+                element = element.replace(/ /g,"_");
+
+                let tag = element + 'Price';
+                
+                let price = obj[tag];
+                
+            
+
+                template += '<label for="'+ element +'Price">' + element + ' Price:' + '</label><input type="text" name="'+ tag +'" value="'+ price +'"><br>';
+            });
+        }
+        else{
+            roles.forEach(function(element){
+                let tag = element + 'Price';
+                template += '<label for="'+ element +'Price">' + element + ' Price:' + '</label><input type="text" name="'+ tag +'"><br>';
+            });
+        }
+
+        
+
+
+        $('.appendix').remove();
+        $('#post-' + this.name).after
+            (
+                '<tr class="appendix">' + 
+                '<td class="column-name has-row-actions column-primary">' +
+                '<form method="POST" action="">' +
+                    template + 
+                    '<br>' + 
+                    '<input type="hidden" value="'+this.name+'" name="productID" ></input>' +
+                    '<input type="submit" name="editPrices" class="button-primary" value="OK"></form>' +  
+                    '</td>' +
+                '</tr>' 
+                
+            )
     });
 });
